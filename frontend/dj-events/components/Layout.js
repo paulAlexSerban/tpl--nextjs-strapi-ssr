4b788c3e8dd23bac860c118d6fe39f28be1b2d4b
paulAlexSerban <paul.alex.serban@gmail.com>
2022-09-12 15:42:11 +0300

    feat: wip

---
+++ frontend/dj-events/components/Layout.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import styles from "@/styles/layout.module.scss";
+import { useRouter } from "next/router";
+import Header from "./Header";
+import Footer from "./Footer";
+import Showcase from "./Showcase";
+
+export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+  
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="{description}" />
+        <meta name="keywords" content="{keywords}" />
+      </Head>
+      <Header />
+      {router.pathname === '/' ?? <Showcase />} {/* 1 */}
+      <div className={styles.layout__container}>{children}</div>
+      <Footer />
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "DJ Events | Find the hottest parties",
+  description: "Find the latest DJ and other musical events",
+  keywords: "music, dj, events, edm",
+};
+
+/**
+ * @notes
+ * 1. `&&` is used as a shorthand, instead of of using a { condition ? do something : do something else}
+ */
---
+++ frontend/dj-events/pages/events/[slug].js
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout';
+
+export default function EventsPage() {
+  return (
+    <Layout title="Single event">
+      <h1>My single event</h1>
+    </Layout>
+  );
+}
