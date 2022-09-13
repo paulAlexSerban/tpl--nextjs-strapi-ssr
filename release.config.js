93a331dd9aeb0ff8b1788a22d4aa968e5d44c069
paulAlexSerban <paul.alex.serban@gmail.com>
2022-09-13 10:49:05 +0300

    feat(wip): add initial strapi backend codebase

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "release",
-  repositoryUrl: "https://github.com/paulAlexSerban/tpl--nextjs",
+  repositoryUrl: "https://github.com/paulAlexSerban/tpl--nextjs-ssr",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
