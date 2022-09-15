50b39fa06d302434e5b1286258f1791770a48a17
paulAlexSerban <paul.alex.serban@gmail.com>
2022-09-16 00:35:36 +0300

    feat(backend): fix add event functinality

--- frontend/dj-events/pages/events/add.js
+++ frontend/dj-events/pages/events/add.js
@@ -4,9 +4,9 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/form.module.scss";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { parseCookies } from '@/helpers/index';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { parseCookies } from "@/helpers/index";
 
 /**
  * @name: Add Event
@@ -27,15 +27,13 @@ export default function AddEventPage() {
   const router = useRouter();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     // Validation
-    const hasEmptyFields = Object.values(values).some(
-      (element) => element === ''
-    )
+    const hasEmptyFields = Object.values(values).some((element) => element === "");
 
     if (hasEmptyFields) {
-      toast.error('Please fill in all fields')
+      toast.error("Please fill in all fields");
     }
 
     const res = await fetch(`${API_URL}/api/events`, {
@@ -46,107 +44,78 @@ export default function AddEventPage() {
       body: JSON.stringify({ data: values }),
     });
 
-    // if (!res.ok) {
-    //   if (res.status === 403 || res.status === 401) {
-    //     toast.error('No token included')
-    //     return
-    //   }
-    //   toast.error('Something Went Wrong')
-    // } else {
-    //   const evt = await res.json()
-    //   router.push(`/events/${evt.slug}`)
-    // }
-  }
+    if (!res.ok) {
+      if (res.status === 403 || res.status === 401) {
+        toast.error("No token included");
+        return;
+      }
+      toast.error("Something Went Wrong");
+    } else {
+      const evt = await res.json();
+      const slug = evt.data.attributes.slug;
+      router.push(`/events/${slug}`);
+    }
+  };
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target
-    setValues({ ...values, [name]: value })
-  }
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
 
   return (
-    <Layout title='Add New Event'>
-      <Link href='/events'>Go Back</Link>
+    <Layout title="Add New Event">
+      <Link href="/events">Go Back</Link>
       <h1>Add Event</h1>
       <ToastContainer />
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.grid}>
           <div>
-            <label htmlFor='name'>Event Name</label>
-            <input
-              type='text'
-              id='name'
-              name='name'
-              value={values.name}
-              onChange={handleInputChange}
-            />
+            <label htmlFor="name">Event Name</label>
+            <input type="text" id="name" name="name" value={values.name} onChange={handleInputChange} />
           </div>
           <div>
-            <label htmlFor='performers'>Performers</label>
+            <label htmlFor="performers">Performers</label>
             <input
-              type='text'
-              name='performers'
-              id='performers'
+              type="text"
+              name="performers"
+              id="performers"
               value={values.performers}
               onChange={handleInputChange}
             />
           </div>
           <div>
-            <label htmlFor='venue'>Venue</label>
-            <input
-              type='text'
-              name='venue'
-              id='venue'
-              value={values.venue}
-              onChange={handleInputChange}
-            />
+            <label htmlFor="venue">Venue</label>
+            <input type="text" name="venue" id="venue" value={values.venue} onChange={handleInputChange} />
           </div>
           <div>
-            <label htmlFor='address'>Address</label>
-            <input
-              type='text'
-              name='address'
-              id='address'
-              value={values.address}
-              onChange={handleInputChange}
-            />
+            <label htmlFor="address">Address</label>
+            <input type="text" name="address" id="address" value={values.address} onChange={handleInputChange} />
           </div>
           <div>
-            <label htmlFor='date'>Date</label>
-            <input
-              type='date'
-              name='date'
-              id='date'
-              value={values.date}
-              onChange={handleInputChange}
-            />
+            <label htmlFor="date">Date</label>
+            <input type="date" name="date" id="date" value={values.date} onChange={handleInputChange} />
           </div>
           <div>
-            <label htmlFor='time'>Time</label>
-            <input
-              type='text'
-              name='time'
-              id='time'
-              value={values.time}
-              onChange={handleInputChange}
-            />
+            <label htmlFor="time">Time</label>
+            <input type="text" name="time" id="time" value={values.time} onChange={handleInputChange} />
           </div>
         </div>
 
         <div>
-          <label htmlFor='description'>Event Description</label>
+          <label htmlFor="description">Event Description</label>
           <textarea
-            type='text'
-            name='description'
-            id='description'
+            type="text"
+            name="description"
+            id="description"
             value={values.description}
             onChange={handleInputChange}
           ></textarea>
         </div>
 
-        <input type='submit' value='Add Event' className='btn' />
+        <input type="submit" value="Add Event" className="btn" />
       </form>
     </Layout>
-  )
+  );
 }
 
 // export async function getServerSideProps({ req }) {
